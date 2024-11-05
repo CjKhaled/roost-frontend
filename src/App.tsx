2ca6d6ac6273d0b8ccf6fdb22ffa872261073924
Sebastian Sosa <1sebastian1sosa1@gmail.com>
2024-11-05 16:57:14 -0500

    untested websocket setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,45 @@
+import React, { useEffect, useState } from 'react'
+import {io, Socket} from 'socket.io-client'
 import { Outlet } from 'react-router-dom'
 
+const SOCKET_URL = 'http://localhost:3000'
+
 const App = (): JSX.Element => {
+  const [socket, setSocket] = useState<Socket | null>(null)
+
+  useEffect(() => {
+    const socketIo = io(SOCKET_URL)
+    setSocket(socketIo)
+
+    return () => {
+      socketIo.disconnect()
+    }
+  }, [])
+
+  // handle events
+  useEffect(() => {
+    if (socket) {
+      socket.on('recieveMessage', (message) => {
+        console.log("Message recieved: ", message)
+      })
+      socket.on('typing', (data) => {
+        console.log(`${data.username} is typing...`)
+      })
+      socket.on('disconnect', () => {
+        console.log("Disconnected from server")
+      })
+    }
+  }, [socket])
+
+  const sendMessage = (message: string) => {
+    if (socket) {
+      socket.emit('sendMessage', {
+        text: message,
+        userId: 'SOME_USER_ID'
+      })
+    }
+  }
+
   return (
     <div>
         <Outlet />
