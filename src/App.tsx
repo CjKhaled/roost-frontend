5cdcc5f8a1b91e23d2c24a0dd6815ea043dd05ee
Sebastian Sosa <1sebastian1sosa1@gmail.com>
2024-11-21 00:57:02 -0500

    clean temp code

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
 import { SocketProvider } from './context/SocketContext'
-import ChatComponent from './components/ui/Chat'
 
 const App = (): JSX.Element => {
   return (
     <AuthProvider>
       <SocketProvider>
         <Outlet />
-        <ChatComponent recipientId='2' conversationId='1' />
       </SocketProvider>
     </AuthProvider>
   )
